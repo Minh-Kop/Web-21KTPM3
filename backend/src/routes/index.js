cc175ee861815bad0155240d4692d96437555ef0
20127538 <87182623+Minh-Kop@users.noreply.github.com>
2023-11-30 21:46:55 +0700

    Add project structure

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 // const authController = require('../controllers/authController');
 const categoryRouter = require('./categoryRoutes');
 const bookRouter = require('./bookRoutes');
+const bookRouterUI = require('./bookRoutesUI');
 const userRouter = require('./userRoutes');
 // const cartRouter = require('./cartRoutes');
 // const voucherRouter = require('./voucherRoutes');
@@ -19,6 +20,7 @@ const router = express.Router();
 
 router.use('/api/category', categoryRouter);
 router.use('/api/books', bookRouter);
+router.use('/books', bookRouterUI);
 router.use('/api/users', userRouter);
 // router.use('/api/cart', authController.protect, cartRouter);
 // router.use('/api/voucher', authController.protect, voucherRouter);
