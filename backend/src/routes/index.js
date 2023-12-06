7e9319bb46c11ace9149d008513d026478db6f8c
20127538 <87182623+Minh-Kop@users.noreply.github.com>
2023-12-06 17:40:13 +0700

    Update jwt to store UserId instead of email

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-// const authController = require('../controllers/authController');
+const authController = require('../controllers/authController');
 const categoryRouter = require('./categoryRoutes');
 const bookRouter = require('./bookRoutes');
 const userRouter = require('./userRoutes');
-// const cartRouter = require('./cartRoutes');
+const cartRouter = require('./cartRoutes');
 // const voucherRouter = require('./voucherRoutes');
 // const locationRouter = require('./locationRoutes');
 // const shippingAddressRouter = require('./shippingAddressRoutes');
@@ -20,7 +20,7 @@ const router = express.Router();
 router.use('/api/category', categoryRouter);
 router.use('/api/books', bookRouter);
 router.use('/api/users', userRouter);
-// router.use('/api/cart', authController.protect, cartRouter);
+router.use('/api/cart', authController.protect, cartRouter);
 // router.use('/api/voucher', authController.protect, voucherRouter);
 // router.use('/api/location', locationRouter);
 // router.use(
