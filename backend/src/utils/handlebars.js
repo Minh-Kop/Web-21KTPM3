9bd39fc71e84cd9dce340fb12458ed5d451fd4b8
20127538 <87182623+Minh-Kop@users.noreply.github.com>
2023-12-19 08:01:46 +0700

    Finish category section in category page

--- backend/src/utils/handlebars.js
+++ backend/src/utils/handlebars.js
@@ -1,5 +1,48 @@
 const Handlebars = require('handlebars');
 
+function createCategoryTree(catId, selectedBranch, options) {
+    const { isTerminationPoint } = options;
+    let html = '';
+
+    selectedBranch = Array.isArray(selectedBranch)
+        ? selectedBranch
+        : [selectedBranch];
+
+    selectedBranch.forEach((el) => {
+        const { isParentOfTerminationPoint } = el;
+        let isActive;
+
+        if (isParentOfTerminationPoint) {
+            isActive = 'parent-active';
+            options.isTerminationPoint = true;
+        } else if (catId === el.id) {
+            isActive = isTerminationPoint ? 'child-active' : 'active';
+        }
+
+        let childHtml = '';
+        if (el.children) {
+            childHtml = createCategoryTree(catId, el.children, options);
+        }
+
+        html += `
+            <li class='${isActive}'>
+                <a
+                    class='category-section__list__link'
+                    href='#!'
+                >
+                    ${el.categoryName}
+                </a>
+                ${childHtml}
+            </li>
+        `;
+    });
+
+    if (html !== '') {
+        html = `<ul class='category-section__list'>${html}</ul>`;
+    }
+    return html;
+}
+
 module.exports = (hbs) => {
     return hbs.create({
         extname: '.hbs',
@@ -23,6 +66,7 @@ module.exports = (hbs) => {
 
                 return result;
             },
+            createCategoryTree,
         },
     });
 };
