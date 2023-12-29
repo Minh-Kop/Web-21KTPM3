e31ae995cc830ef9da724cdcb42d16694027528b
20127544 <tuankietnguyen275@gmail.com>
2023-12-29 21:04:17 +0700

    update

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,6 +7,7 @@ const categoryRouterUI = require('./categoryRoutesUI');
 const bookRouter = require('./bookRoutes');
 const bookRouterUI = require('./bookRoutesUI');
 const userRouter = require('./userRoutes');
+const userRouterUI = require('./userRoutesUI');
 const cartRouter = require('./cartRoutes');
 const cartRouterUI = require('./cartRoutesUI');
 const mainPage = require('../controllers/bookControllerUI');
@@ -14,9 +15,11 @@ const mainPage = require('../controllers/bookControllerUI');
 // const voucherRouter = require('./voucherRoutes');
 // const locationRouter = require('./locationRoutes');
 const shippingAddressRouter = require('./shippingAddressRoutes');
+const shippingAddressRouterUI = require('./shippingAddressRoutesUI');
 // const checkoutRouter = require('./checkoutRoutes');
 // const paymentRouter = require('./paymentRoutes');
 const orderRouter = require('./orderRoutes');
+const orderRouterUI = require('./orderRoutesUI');
 // const reviewRouter = require('./reviewRoutes');
 // const searchRouter = require('./searchRoutes');
 // const pointRouter = require('./pointRoutes');
@@ -29,6 +32,7 @@ router.use('/category', categoryRouterUI);
 router.use('/api/books', bookRouter);
 router.use('/books', bookRouterUI);
 router.use('/api/users', userRouter);
+router.use('/users', userRouterUI);
 router.use('/api/cart', authController.protect, cartRouter);
 router.use('/cart', authController.protectPage, cartRouterUI);
 router.use('/mainPage', mainPage.renderMainPage);
@@ -39,9 +43,15 @@ router.use(
     authController.protect,
     shippingAddressRouter,
 );
+router.use(
+    '/shippingAddress',
+    authController.protectPage,
+    shippingAddressRouterUI,
+);
 // router.use('/api/checkout', checkoutRouter);
 // router.use('/api/payment', paymentRouter);
 router.use('/api/order', authController.protect, orderRouter);
+router.use('/order', authController.protectPage, orderRouterUI);
 // router.use('/api/review', reviewRouter);
 // router.use('/api/search', searchRouter);
 // router.use('/api/point', authController.protect, pointRouter);
